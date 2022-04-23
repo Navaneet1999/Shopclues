368a497699244f14e872b9941e95c4fb3e83a964
Navaneet1999 <93372592+Navaneet1999@users.noreply.github.com>
2022-04-24 01:16:40 +0530

    updat

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom"
 import { addCartProduct } from "../Redux/action";
 
 export const Cart = () => {
     const [cart,setCart] = useState([])
     const {prodId} = useParams();
-    let product = useSelector((state)=> state.cartProduct.products);
-    const {image, title, price} = product;
+    //let product = useSelector((state)=> state.cartProduct.products);
+    // const {image, title, price} = product;
     const fetchCart = async(id) => {
         const res = await axios
         .get(`https://fakestoreapi.com/products/${id}`)
